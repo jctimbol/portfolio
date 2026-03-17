e35d3694d1c1696a7362afa314c8a60618ee68c8
Jay Timbol <jlc.timbol@gmail.com>
2026-03-16 22:44:05 -0700

    im still workingonthis i swear

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "portfolio",
+  title: "Portfolio",
 };
 
 export default function RootLayout({
@@ -13,8 +13,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script dangerouslySetInnerHTML={{ __html: 'history.scrollRestoration="manual";window.scrollTo(0,0);' }} />
-
+        <script
+          dangerouslySetInnerHTML={{
+            __html: 'history.scrollRestoration="manual";window.scrollTo(0,0);',
+          }}
+        />
       </head>
       <body>{children}</body>
     </html>
